88af10aa38017915f1e73b6676cc4ae73f7910ca
Ziwei <ziweichen88@gmail.com>
2013-08-06 14:47:42 +0200

    script fixed

--- script/async.sh
+++ script/async.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 now="$(date +%s%N)"
 
-ab -g "/home/ziwei/workspace/KompicsSRE/script/result/plot.dat" -n 201 -c 1 -p "/home/ziwei/workspace/KompicsSRE/script/asyncbody" -T 'application/json' "http://localhost:8080/SRE/sics.vision.ExampleStorlet/handlerE/$now" 
+ab -g "/home/ziwei/workspace/KompicsSRE/script/result/plot.dat" -n 1000 -c 1 -p "/home/ziwei/workspace/KompicsSRE/script/asyncbody" -H "Accept: application/json" "http://localhost:8080/SRE/sics.vision.ExampleStorlet/handlerE/$now" 
--- script/sync.sh
+++ script/sync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 now="$(date +%s%N)"
 
-ab -n 1000 -c 100 -p "/home/ziwei/workspace/KompicsSRE/script/syncbody" -T 'application/json' "http://localhost:8080/SRE/syncStorlet"
+ab -n 1000 -c 100 -p "/home/ziwei/workspace/KompicsSRE/script/syncbody" -H "Accept: application/json" "http://localhost:8080/SRE/syncStorlet"
 
