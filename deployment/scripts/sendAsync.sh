6d1b2b8d195c9073aebef7b1c81f804a1110275e
Ziwei <ziweichen88@gmail.com>
2013-09-16 17:06:28 +0200

    non-source files

---
+++ deployment/scripts/sendAsync.sh
@@ -0,0 +1,1 @@
+curl -i -X POST "http://localhost:8080/SRE/TestTenant.edt-testChain6.RaiUnzipStorlet/zipupload/123" -H "Accept: application/json" --data-binary "@asyncbody"
---
+++ deployment/scripts/sendSync.sh
@@ -0,0 +1,1 @@
+curl -i -X POST "http://localhost:8080/SRE/syncStorlet" -H "Accept: application/json" --data-binary "@syncbody"
