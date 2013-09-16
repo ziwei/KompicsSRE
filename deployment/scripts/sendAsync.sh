82a58920047ff4c1ef71a0bc756ac6ba4d084910
Ziwei <ziweichen88@gmail.com>
2013-09-16 17:07:37 +0200

    remove scripts

--- deployment/scripts/sendAsync.sh
+++
@@ -1,1 +0,0 @@
-curl -i -X POST "http://localhost:8080/SRE/TestTenant.edt-testChain6.RaiUnzipStorlet/zipupload/123" -H "Accept: application/json" --data-binary "@asyncbody"
--- deployment/scripts/sendSync.sh
+++
@@ -1,1 +0,0 @@
-curl -i -X POST "http://localhost:8080/SRE/syncStorlet" -H "Accept: application/json" --data-binary "@syncbody"
